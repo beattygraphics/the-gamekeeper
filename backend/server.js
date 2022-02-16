380eda3faaadb08be5aa6b46e771ad9a4f8acb4e
Erin Beatty <beattygraphics@icloud.com>
2022-02-16 18:18:58 -0500

    update package. json file

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,13 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
 })
